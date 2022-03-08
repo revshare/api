0b55a914cfa92a426c56a9045697e00c29064544
Reuben Baltazar <arvia617@gmail.com>
2022-03-08 14:42:49 +0800

    galing laptop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    lorem ipsum
+                                    lorem ipsum galing sa laptop
                                 </div>
                             </div>
                         </div>
